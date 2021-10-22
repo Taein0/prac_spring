0330a747a1fd9762695357484d0c7e87ae706a21
karte000 <karte000@gmail.com>
2021-10-22 15:20:54 +0900

    [10/22]메일 인증 추가

--- spring06_toyProject/src/main/java/com/kh/spring/member/model/service/MemberService.java
+++ spring06_toyProject/src/main/java/com/kh/spring/member/model/service/MemberService.java
@@ -1,22 +1,32 @@
 package com.kh.spring.member.model.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
+import com.kh.spring.code.Config;
+import com.kh.spring.common.mail.MailSender;
 import com.kh.spring.member.model.dto.Member;
 import com.kh.spring.member.model.repository.MemberRepository;
 import com.kh.spring.member.validator.JoinForm;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class MemberService {
 	
-	@Autowired
-	private MemberRepository memberRepository;
-	
-	
+	private final MemberRepository memberRepository;
+	private final MailSender mailSender;
+	private final RestTemplate http;	
+	private final PasswordEncoder passwordEncoder;
 
 	public void insertMember(JoinForm form) {
-
+		form.setPassword(passwordEncoder.encode(form.getPassword()));
 		memberRepository.insertMebmer(form);
 		
 	}
@@ -24,8 +34,13 @@ public class MemberService {
 
 
 	public Member authenicateUser(Member member) {
+		Member storedMember = memberRepository.selectMemberByUserId(member.getUserId());
 		
-		return memberRepository.authenicateUser(member);
+		if(passwordEncoder.matches(member.getPassword(), storedMember.getPassword())) {
+			return storedMember;
+		}
+		
+		return null;
 	}
 
 
@@ -33,5 +48,25 @@ public class MemberService {
 	public Member selectMemberByUserId(String userId) {
 		
 		return memberRepository.selectMemberByUserId(userId);
+	}
+
+
+
+	public void authenicateByEmail(JoinForm form, String token) {
+		MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+		body.add("mailTemplate", "join-auth-mail");
+		body.add("userId", form.getUserId());
+		body.add("persistToken", token);
+		
+		//RestTemplate의 기본 Content-type : application/json
+		
+		RequestEntity<MultiValueMap<String, String>> request =
+				RequestEntity.post(Config.DOMAIN.DESC+"/mail")
+				.accept(MediaType.APPLICATION_FORM_URLENCODED)
+				.body(body);
+		
+		String htmlTxt = http.exchange(request, String.class).getBody();
+		mailSender.send(form.getEmail(), "회원가입을 축하합니다.",htmlTxt);
+		
 	}
 }
